b9102fac32cc09eafc6bcab6aa30fe1da7454534
Myriam Lubrano <myriam.lubrano2@unibo.it>
2026-01-02 13:08:34 +0100

    Refactor Mitschke_2018 function with author info

--- Algorithms/Mitschke_2018.m
+++ Algorithms/Mitschke_2018.m
@@ -1,7 +1,8 @@
 function [IC,TO] = Mitschke_2018(acc,gyr,fs,MS)
 
 % -------------------------------------------------------------------------
-% Mitschke_2018
+% This implementation is based on the methodology described in Mitschke, 2018.
+% The original authors of the referenced papers are not responsible for this implementation.
 %
 % IC and TO detection from acceleration and angular velocity
 % Windowed in mid-swing to mid-swing cycles
@@ -15,6 +16,8 @@ function [IC,TO] = Mitschke_2018(acc,gyr,fs,MS)
 % OUTPUT:
 %   IC          detected initial contacts
 %   TO          detected toe offs
+%
+% Author: Myriam Lubrano
 % -------------------------------------------------------------------------
 
 IC = [];
@@ -96,4 +99,5 @@ IC = IC(:);
 TO = TO(:);
 
 end
+
 
